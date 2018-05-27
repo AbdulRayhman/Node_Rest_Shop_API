a09277bc719fb361521f4084ceda6bf7ca3eb151
Abdulrayhman <s.abderrehman@gmail.com>
2018-05-27 18:43:27 +0500

    Add Packages

---
+++ modals/UserModal.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const UserSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/)
+});
+module.exports = mongoose.model('UserModal', UserSchema)
